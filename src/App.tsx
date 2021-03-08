9e2043bc57e3ff223aa1b00cc2282f3c955ac9d1
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-03-08 23:04:04 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+
+import Todolist from "./Todolist";
+
+export type TaskType = {
+    id: number;
+    title: string;
+    isDone: boolean;
+};
+
+const taskOne: Array<TaskType> = [
+    { id: 1, title: "HTML", isDone: true },
+    { id: 2, title: "CSS", isDone: true },
+    { id: 3, title: "React", isDone: false },
+];
+
+const taskTwo: Array<TaskType> = [
+    { id: 1, title: "milk", isDone: true },
+    { id: 2, title: "bread", isDone: true },
+    { id: 3, title: "meat", isDone: false },
+];
+
+function App() {
+    return (
+        <div className="App">
+            <Todolist title={"first"} tasks={taskOne} />
+            <Todolist title={"second"} tasks={taskTwo} />
+        </div>
+    );
+}
+
+export default App;
